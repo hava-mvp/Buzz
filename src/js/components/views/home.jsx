04861297ddc99893b6dc240e7c22ff74e4a92268
Conor C <Conorc1000@gmail.com>
2016-02-07 18:40:17 +0000

    code swapped

--- src/js/components/views/home.jsx
+++ src/js/components/views/home.jsx
@@ -2,19 +2,13 @@ import React from 'react';
 import Formsy from 'formsy-react';
 import { Input } from 'formsy-react-components';
 
-let NavBar     = require('../navbar.jsx');
-let Content    = require('../content.jsx');
-let HowItWorks = require('../how-it-works.jsx');
-let Social     = require('../social-media.jsx');
 
 let Home = React.createClass({
   render: function() {
     return (
       <div>
-        <NavBar />
-        <Content url="/submitted" />
-        <HowItWorks />
-        <Social />
+        <h1>Hava</h1>
+        {this.props.children}
       </div>
     );
   }
