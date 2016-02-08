4e77c95ef1d75dbe362f69b9a133601f48f80b54
Ruth <emailtoruth@virginmedia.com>
2016-02-08 14:35:36 +0000

    adding to see when heroku breaks

--- src/js/components/BarLogin.jsx
+++ src/js/components/BarLogin.jsx
@@ -57,58 +57,13 @@ var BarLogin = React.createClass({
   render: function() {
     return (
       <div>
-
-        <div className='form-group'>
-        <form id='sign_up'>
-          <div className='input-container'>
-            <div className='row'>
-              <input
-                className='form-control box-spacing'
-                id="first_name"
-                name='landing[first_name]'
-                placeholder='First name'
-                required type='text'
-              />
-              <input
-                className='form-control box-spacing'
-                id="last_name"
-                placeholder='Last name'
-                required type='text'
-              />
-            </div>
-            <div className='row'>
-              <input
-                className='form-control box-spacing'
-                id='email'
-                placeholder='Your email'
-                required type='email'
-              />
-              <input
-                className='form-control box-spacing'
-                id='postcode'
-                placeholder='Your postcode'
-                required type='text'
-              />
-            </div>
-          </div>
-          <div className='row'>
-            <div className='dropdown-container'>
-              <div id='select_arrow'></div>
-              <select className='btn drop' id='survey'>
-                <option value>How did you first hear about Hava?</option>
-                <option value='Friends or Family'>Friends or Family</option>
-                <option value='Social Media'>Social Media</option>
-                <option value='Search Engine'>Search Engine</option>
-                <option value='Meet-up or Conference'>Meet-up or Conference</option>
-                <option value='Online Ad'>Online Ad</option>
-                <option value='Other'>Other</option>
-              </select>
-            </div>
-          </div>
-          <button type='submit' className='btn btn-md submit'>Submit your details!</button>
-        </form>
-      </div>
-
+        <div className='wrapper'>
+          <h2>Hava Bar Login</h2>
+          <input className='form-control' id="barName" placeholder='Enter the name of your bar' required type='text'/>
+          <input className='form-control' id="email" placeholder='Enter email' required type='email' />
+          <input className='form-control' id='password' required type='password'/>
+          <button id='button' className='btn btn-md button'>Submit your details!</button>
+        </div>
       </div>
     )
   }
