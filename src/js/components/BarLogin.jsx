f6cb336994cb6f3586eb973de303e4a0a4c5aca5
Ruth <emailtoruth@virginmedia.com>
2016-02-08 14:00:54 +0000

    adding to see when heroku breaks

--- src/js/components/BarLogin.jsx
+++ src/js/components/BarLogin.jsx
@@ -57,28 +57,14 @@ var BarLogin = React.createClass({
   render: function() {
     return (
       <div>
-         <h2> Bar Login</h2>
-            <div>
-              <label for="txtRegEmail">Email address</label>
-              <input value="conorc1000@gmail.com" type="email" id="email" placeholder="Enter email" name="email" />
-            </div>
-            <div>
-              <label>Bar Name</label>
-              <input value="The Nag's Head" placeholder="Enter Bar Name" id="barName" />
-            </div>
-            <div>
-              <label for="txtRegPassword">Password</label>
-              <input type="password" id="password" placeholder="password" />
-            </div>
-          <button id="button">Login</button>
 
-            <div className="wrapper">
-               <h2>Hava Bar Login</h2>
-               <input type="text" label="Bar Name" placeholder="Enter the name of your bar" id="barName"/>
-               <input type="email" label="Email Address" placeholder="Enter email" id="email" />
-               <input type="password" label="Password" id="password"/>
-               <button id="button" className="button"/>Login</button>
-            </div>
+        <div className="wrapper">
+           <h2>Hava Bar Login</h2>
+           <input type="text" label="Bar Name" placeholder="Enter the name of your bar" id="barName"/>
+           <input type="email" label="Email Address" placeholder="Enter email" id="email" />
+           <input type="password" label="Password" id="password"/>
+           <button id="button" className="button">Login</button>
+        </div>
 
       </div>
     )
