79bcc6f2514f52cc71ee156239836db26792a5fb
Ruth <emailtoruth@virginmedia.com>
2016-02-08 13:28:13 +0000

    adding to see when heroku breaks

--- src/js/components/BarLogin.jsx
+++ src/js/components/BarLogin.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Firebase from 'firebase';
 import { Router, Route, Link } from 'react-router';
 import CreateOffers from './CreateOffers.jsx';
+import { Input, ButtonInput } from 'react-bootstrap';
 
 var firebaseRef = new Firebase("https://havamvp.firebaseio.com/customer");
 
@@ -57,20 +58,14 @@ var BarLogin = React.createClass({
   render: function() {
     return (
       <div>
-         <h2> Bar Login</h2>
-            <div>
-              <label for="txtRegEmail">Email address</label>
-              <input value="conorc1000@gmail.com" type="email" id="email" placeholder="Enter email" name="email" />
-            </div>
-            <div>
-              <label>Bar Name</label>
-              <input value="The Nag's Head" placeholder="Enter Bar Name" id="barName" />
-            </div>
-            <div>
-              <label for="txtRegPassword">Password</label>
-              <input type="password" id="password" placeholder="password" />
-            </div>
-          <button id="button">Login</button>
+
+       <div className="wrapper">
+         <h2>Hava Bar Login</h2>
+         <Input type="text" label="Bar Name" placeholder="Enter the name of your bar" id="barName"/>
+         <Input type="email" label="Email Address" placeholder="Enter email" id="email" />
+         <Input type="password" label="Password" id="password"/>
+         <ButtonInput value="Login" id="button" className="button"/>
+       </div>
 
       </div>
     )
