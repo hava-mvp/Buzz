d959966979350c2df3963cba312ee1ff7f06286a
Conor C <Conorc1000@gmail.com>
2016-02-11 10:44:11 +0000

    nav bar

--- src/js/components/nav/NavBar.jsx
+++ src/js/components/nav/NavBar.jsx
@@ -8,21 +8,39 @@ var NavBar = React.createClass({
     document.getElementById('contactBtn').addEventListener('click', function(){
       window.location.assign("/public/#/customer-contact");
     })
+
+    document.getElementById('backBtn').addEventListener('click', function(){
+      window.location.assign("/public/#/live-offers");
+    })
   },
 
   render : function (){
-
-    return (
-      <div>
-        <nav className="navbar navbar-default">
-          <div className="navbar-header">
-            <a className="navbar-brand" href="#">Hava</a>
-            <button type="submit" id="contactBtn" className="btn btn-default">Contact</button>
-          </div>
-        </nav>
-      </div>
-    )
+    if (true){
+      return (
+        <div>
+          <nav className="navbar navbar-default">
+            <div className="navbar-header">
+              <a className="navbar-brand" href="#">Hava</a>
+              <button type="submit" id="contactBtn" className="btn btn-default">Contact</button>
+            </div>
+          </nav>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <nav className="navbar navbar-default">
+            <div className="navbar-header">
+              <a className="navbar-brand" href="#">Hava</a>
+              <button type="submit" id="customerLoginBtn" className="btn btn-default">Back</button>
+            </div>
+          </nav>
+        </div>
+      )
+    }
   }
 });
 
 module.exports = NavBar;
+
+//window.location.match
