1b93e5775bda0b4cda3341ab32584cbd4456fafc
Ruth <emailtoruth@virginmedia.com>
2016-02-11 18:16:07 +0000

    code toggle wip #19

--- src/js/components/IndividualOffer.jsx
+++ src/js/components/IndividualOffer.jsx
@@ -7,8 +7,12 @@ var IndividualOffer = React.createClass({
     var buttons = document.getElementsByClassName('show-code-button');
     for (var i = 0; i < buttons.length; i++){
       buttons[i].addEventListener('click', function(){
-        var id = this.outerHTML.split('id="')[1].split('"')[0];
-        document.getElementById(id).innerHTML = 'CODEEEEE'
+        var buttonId = this.outerHTML.split('id="')[1].split('"')[0];
+        var pId = self.props.offerDetails.offerCode
+        console.log(pId);
+        console.log(buttonId);
+        document.getElementById(buttonId).setAttribute("style", "display:none")
+        document.getElementById(pId).setAttribute("style", "display:block")
       });
     }
   },
@@ -26,8 +30,9 @@ var IndividualOffer = React.createClass({
         <div className="info">
           End Time: {this.props.offerDetails.endTime}
         </div>
-        <div id={this.props.offerDetails.barName.replace(/\"/g,"")} className="info">
-          <button id={this.props.offerDetails.barName.replace(/\"/g,"")} className="show-code-button btn btn-xs">Show Offer Code</button>
+        <div className="info">
+          <button id={this.props.offerDetails.barName.replace(/\"/g,"")} className="show-code-button btn btn-xs">Show Code</button>
+          <p id={this.props.offerDetails.offerCode} className="code">{this.props.offerDetails.offerCode}</p>
         </div>
       </li>
     )
