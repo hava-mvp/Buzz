f29fa3e10dfb7ececee32095d231b0e2167cb864
Marie Kasai <marie.kasai@live.co.uk>
2016-02-13 09:45:54 +0000

    maps to bar fixed?

--- src/js/components/IndividualOffer.jsx
+++ src/js/components/IndividualOffer.jsx
@@ -1,76 +1,40 @@
 import React from 'react';
 import Firebase from 'firebase';
-
-
-var getBarURL = (barName, callback) => {
-  firebaseRef.on("value", function(snapshot) {
-    var barObj = snapshot.val();
-
-    for (var key in barObj) {
-       if (barObj.hasOwnProperty(key)) {
-          var obj = barObj[key];
-          for (var prop in obj) {
-             if (obj.hasOwnProperty(prop)) {
-               var objProp = obj[prop]
-
-               if(obj[prop] === barName){
-
-                 callback(obj.mapURL);
-               }
-             }
-          }
-       }
-    }
-  });
-}
+import BarLocation from './BarLocation.jsx'
 
 var IndividualOffer = React.createClass({
 
-  componentWillMount: function (){
-    var _this = this;
-    var liveOfferDetails = new Firebase("https://havamvp.firebaseio.com/bars");
-    
-  },
-
   componentDidMount: function (){
     var self = this;
-    var id = this.props.offerDetails.barName.replace(/\"/g,"")
+    var id = self.props.offerDetails.barName.replace(/\"/g,"")
     document.getElementById(id).addEventListener("click", function(){
       var pId = self.props.offerDetails.offerCode
       document.getElementById(id).setAttribute("style", "display:none")
       document.getElementById(pId).setAttribute("style", "display:block")
-    })
-  },
-
-  handleMapClick: function(){
-    console.log("MAP CLICK");
-    var barName = this.props.offerDetails.barName.replace(/\"/g,"")
-    getBarURL(barName, function(url){
-      var win = window.open(url,'_blank');
     });
   },
 
-
   render: function() {
+    var _this = this;
     return (
     <div>
       <li className="listItem">
         <hr className="line" />
         <div className="barName">
-          {this.props.offerDetails.barName.replace(/\"/g,"")}
+          {_this.props.offerDetails.barName.replace(/\"/g,"")}
         </div>
         <div className="info">
-          {this.props.offerDetails.offer}
+          {_this.props.offerDetails.offer}
         </div>
         <div className="info">
-          End Time: {this.props.offerDetails.endTime}
+          End Time: {_this.props.offerDetails.endTime}
         </div>
         <div className="info">
-          <button id={this.props.offerDetails.barName.replace(/\"/g,"")} className="show-code-button btn btn-lg">Show Code</button>
-          <h3 id={this.props.offerDetails.offerCode} className="code">{this.props.offerDetails.offerCode}</h3>
+          <button id={_this.props.offerDetails.barName.replace(/\"/g,"")} className="show-code-button btn btn-lg">Show Code</button>
+          <h3 id={_this.props.offerDetails.offerCode} className="code">{_this.props.offerDetails.offerCode}</h3>
         </div>
         <div className="info map">
-          <button onClick={this.handleMapClick} id="mapButton" className="show-map-button btn btn-sm">Map</button>
+          <BarLocation offerDetails={_this.props.offerDetails.barName}/>
         </div>
       </li>
     </div>
