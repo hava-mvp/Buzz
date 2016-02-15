917930cc6d3b7857f16b62293348480fa5822bc0
Marie Kasai <marie.kasai@live.co.uk>
2016-02-15 10:30:04 +0000

    map button classes put back

--- src/js/components/BarLocation.jsx
+++ src/js/components/BarLocation.jsx
@@ -20,11 +20,10 @@ var BarLocation = React.createClass({
   render: function() {
     var _this = this;
     return (
-    <div>
       <h5 onClick={_this.barLocationLink} className="show-map-button">Map</h5>
-    </div>
     )
   }
-})
+  
+});
 
 export default BarLocation;
