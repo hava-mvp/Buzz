6447d780dffba8f03d9b33e7e227424bfc189b59
Marie Kasai <marie.kasai@live.co.uk>
2016-02-15 10:22:13 +0000

    buttons component

--- src/js/components/IndividualOffer.jsx
+++ src/js/components/IndividualOffer.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import Firebase from 'firebase';
 import BarLocation from './BarLocation.jsx'
+import ToggleOfferCode from './ToggleOfferCode.jsx'
 
 var IndividualOffer = React.createClass({
-
-  componentDidMount: function (){
-    var self = this;
-    var id = self.props.offerDetails.barName.replace(/\"/g,"")
-    document.getElementById(id).addEventListener("click", function(){
-      var pId = self.props.offerDetails.offerCode
-      document.getElementById(id).setAttribute("style", "display:none")
-      document.getElementById(pId).setAttribute("style", "display:block")
-    });
-  },
 
   render: function() {
     var _this = this;
@@ -27,11 +18,10 @@ var IndividualOffer = React.createClass({
           {_this.props.offerDetails.offer}
         </div>
         <div className="info">
-          End Time: {_this.props.offerDetails.endTime}
+          Offer Expires at: {_this.props.offerDetails.endTime}
         </div>
         <div className="info">
-          <button id={_this.props.offerDetails.barName.replace(/\"/g,"")} className="show-code-button btn btn-lg">Show Code</button>
-          <h3 id={_this.props.offerDetails.offerCode} className="code">{_this.props.offerDetails.offerCode}</h3>
+          <ToggleOfferCode offerDetails={_this.props.offerDetails}/>
         </div>
         <div className="info map">
           <BarLocation offerDetails={_this.props.offerDetails.barName}/>
---
+++ src/js/components/ToggleOfferCode.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Firebase from 'firebase';
+
+var OfferCodeButton = React.createClass({
+
+  componentDidMount: function (){
+    var _this = this;
+    var barName = _this.props.offerDetails.barName.replace(/\"/g,"") ;
+    var barOfferEndTime = _this.props.offerDetails.endTime;
+    var barOfferToggleButton = barName + "-" + barOfferEndTime;
+    document.getElementById(barOfferToggleButton).addEventListener("click", function(){
+      var toggleOffer = _this.props.offerDetails.offerCode;
+      document.getElementById(barOfferToggleButton).setAttribute("style", "display:none")
+      document.getElementById(toggleOffer).setAttribute("style", "display:block")
+    });
+  },
+
+  render: function() {
+    var _this = this;
+    var barName = _this.props.offerDetails.barName.replace(/\"/g,"");
+    var barOfferEndTime = _this.props.offerDetails.endTime;
+    var barOfferToggleButton = barName + "-" + barOfferEndTime;
+    return (
+    <div>
+      <button id={barOfferToggleButton} className="show-code-button btn btn-lg this">Show Code</button>
+      <h3 id={_this.props.offerDetails.offerCode} className="code">{_this.props.offerDetails.offerCode}</h3>
+    </div>
+    )
+  }
+})
+
+export default OfferCodeButton;
