f84242d63854036b6c3fe9c7558b1ac335c69c5e
Marie <marie.kasai@live.co.uk>
2016-02-28 22:53:08 +0000

    fix merge conflicts

--- src/js/components/CustomerContact.jsx
+++ src/js/components/CustomerContact.jsx
@@ -27,7 +27,6 @@ var CustomerContact = React.createClass({
     return (
       <div className="contact-wrapper">
         <h2>Contact Us</h2>
-<<<<<<< HEAD
         <p>
           Email us at:
           <a href="mailto:office@hava-app.com" target="_top"> office@hava-app.com</a>
@@ -44,28 +43,6 @@ var CustomerContact = React.createClass({
           </a>
         </div>
         <ContactFooter navigateTo={'/#live-offers'} footerName={'Back to Live Offers'} />
-=======
-
-          <p>
-            Email us at:&nbsp;
-            <a href="mailto:office@hava-app.com" target="_top">office@hava-app.com</a>
-          </p>
-
-          <div className='rowSocial'>
-            <a href="https://www.instagram.com/thehavaapp/">
-              <img className='icon' src="https://cdn1.iconfinder.com/data/icons/iconza-circle-social/64/697067-instagram-512.png"/>
-            </a>
-            <a href="https://www.facebook.com/havaapp/">
-              <img className='icon' src="https://cdn1.iconfinder.com/data/icons/iconza-circle-social/64/697057-facebook-512.png"/>
-            </a>
-            <a href="https://twitter.com/thehavaapp">
-              <img className='icon' src="https://cdn1.iconfinder.com/data/icons/iconza-circle-social/64/697029-twitter-512.png"/>
-            </a>
-          </div>
-          <div className="site-footer offer-footer">
-            <p onClick={this.handleBackClick} id="contactBtn" className="footer-text">Back to Live Offers</p>
-          </div>
->>>>>>> 1c7ea19c5449129a65723c4951f3895de3baa92a
       </div>
     )
   }
